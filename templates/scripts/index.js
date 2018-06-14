0807d1a5b1243d2677e5a34d28e7e7327fccc80b
Charlie Jackson <charlie@charliejackson.com>
2018-06-14 18:33:57 +0100

    Show help on no command

--- templates/scripts/index.js
+++ templates/scripts/index.js
@@ -3,8 +3,21 @@
 const program = require('commander');
 const config = require('../../package.json');
 
-program.version(config.version);
+let cmdValue;
+
+program
+  .version(config.version)
+  .arguments('<cmd> [options]')
+  .action((cmd) => {
+    cmdValue = cmd;
+  });
 
 require('./init');
 
 program.parse(process.argv);
+
+if (!cmdValue) {
+  console.error('\nNo command given! See the help below');
+  program.outputHelp();
+  process.exit(1);
+}
