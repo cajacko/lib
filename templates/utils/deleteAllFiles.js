89bd07964e53aa15c332c09f498b0da85dedfaf8
Charlie Jackson <charlie@charliejackson.com>
2018-06-14 19:52:47 +0100

    Delete all files in dir

--- templates/utils/deleteAllFiles.js
+++ templates/utils/deleteAllFiles.js
@@ -1,2 +1,17 @@
-// TODO:
-module.exports = () => Promise.resolve();
+const rimraf = require('rimraf');
+const globby = require('globby');
+
+const getProjectdir = require('./getProjectDir');
+
+module.exports = (dir) => {
+  const deleteDir = dirToDelete =>
+    globby([`${dirToDelete}/**/*`, `${dirToDelete}/**/.*`]).then((paths) => {
+      paths.forEach((item) => {
+        rimraf.sync(item);
+      });
+    });
+
+  if (dir) return deleteDir(dir);
+
+  return getProjectdir().then(deleteDir);
+};
