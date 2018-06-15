d402474f86b21091c6d386bc9a97612a6e0cd609
Charlie Jackson <charlie@charliejackson.com>
2018-06-15 11:12:50 +0100

    Added git commands

--- templates/utils/git.js
+++ templates/utils/git.js
@@ -1,6 +1,97 @@
 const isGit = require('is-git-repository');
+const inquirer = require('inquirer');
+const { pathExists, ensureDir } = require('fs-extra');
+const { join } = require('path');
+const simpleGit = require('simple-git');
+const getProjectDir = require('./getProjectDir');
+const {
+  getDoesRepoExist,
+  createRepoAndClone,
+  getCanConnectToGithub,
+  setupGitHubConnection,
+} = require('./github');
+
+const createLocalRepo = (name, parentDir) => {
+  const projectDir = join(parentDir, name);
+  return ensureDir(projectDir).then(() => simpleGit(projectDir).init());
+};
+
+const askToResortToLocal = (name, parentDir, message) =>
+  inquirer
+    .prompt([
+      {
+        type: 'confirm',
+        name: 'shouldContinue',
+        message,
+      },
+    ])
+    .then(({ shouldContinue }) => {
+      if (shouldContinue) return createLocalRepo(name, parentDir);
+
+      return Promise.resolve();
+    });
+
+const createAndCloneRepoOrResortToLocal = (name, parentDir) =>
+  getDoesRepoExist(name).then((repoExists) => {
+    if (!repoExists) return createRepoAndClone(name, parentDir);
+
+    return askToResortToLocal(
+      name,
+      parentDir,
+      `The repo "${name}" already exists for the GitHub user. Do you want to create this project as a local only repo?`,
+    );
+  });
 
 exports.getIsGitRepo = () => Promise.resolve(isGit());
 
-// TODO:
-exports.setupNewRepo = () => Promise.resolve();
+exports.setupNewRepo = () =>
+  getProjectDir()
+    .then(projectDir =>
+      inquirer.prompt([
+        {
+          type: 'input',
+          name: 'name',
+          message: 'Repo name',
+          validate: (name) => {
+            if (!name.match(/^[a-z][a-z\-]*[a-z]$/)) {
+              return 'The repo name must be in a url slug like format e.g. repo-name';
+            }
+
+            return true;
+          },
+        },
+        {
+          type: 'input',
+          name: 'parentDir',
+          message:
+            "Where do you want to create this project (don't include the project name)",
+          default: projectDir,
+          validate: parentDir => pathExists(parentDir),
+        },
+        {
+          type: 'confirm',
+          name: 'shouldAddToGitHub',
+          message: 'Do you want to add this as a new repo to GitHub?',
+        },
+      ]))
+    .then(({ name, shouldAddToGitHub, parentDir }) => {
+      if (!shouldAddToGitHub) return createLocalRepo(name, parentDir);
+
+      return getCanConnectToGithub().then((canConnectToGitHub) => {
+        if (canConnectToGitHub) {
+          return createAndCloneRepoOrResortToLocal(name, parentDir);
+        }
+
+        return setupGitHubConnection().then((connectedToGitHub) => {
+          if (connectedToGitHub) {
+            return createAndCloneRepoOrResortToLocal(name, parentDir);
+          }
+
+          return askToResortToLocal(
+            name,
+            parentDir,
+            'Could not establish a connection to GitHub. Do you want to create this project as a local only repo?',
+          );
+        });
+      });
+    });
---
+++ templates/utils/github.js
@@ -0,0 +1,73 @@
+const octokit = require('@octokit/rest')();
+const inquirer = require('inquirer');
+const simpleGit = require('simple-git');
+const { ensureDir } = require('fs-extra');
+const { join } = require('path');
+const settings = require('./settings');
+
+const getCanConnectToGithub = (username, token) => {
+  const gitHubToken = token || settings.get('gitHubToken');
+  const gitHubUsername = username || settings.get('gitHubUsername');
+
+  if (!gitHubToken) return Promise.resolve(false);
+  if (!gitHubUsername) return Promise.resolve(false);
+
+  octokit.authenticate({
+    type: 'token',
+    token: gitHubToken,
+  });
+
+  return octokit.users
+    .getKeysForUser({ username: gitHubUsername })
+    .then(() => true)
+    .catch(() => false);
+};
+
+exports.getCanConnectToGithub = getCanConnectToGithub;
+
+exports.setupGitHubConnection = () => {
+  console.log("You are not connected to GitHub, let's add your details");
+
+  return inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'gitHubUsername',
+        message: 'Enter your gitHub username',
+      },
+      {
+        type: 'input',
+        name: 'gitHubToken',
+        message: 'Enter your gitHub api token',
+      },
+    ])
+    .then(({ gitHubUsername, gitHubToken }) => {
+      settings.set('gitHubToken', gitHubToken);
+      settings.set('gitHubUsername', gitHubUsername);
+
+      return getCanConnectToGithub(gitHubUsername, gitHubToken);
+    });
+};
+
+exports.getDoesRepoExist = (name) => {
+  const gitHubUsername = settings.get('gitHubUsername');
+
+  return octokit.repos
+    .get({ owner: gitHubUsername, repo: name })
+    .then(() => true)
+    .catch(() => false);
+};
+
+const createRepo = name =>
+  octokit.repos.create({ name }).then(repo => repo.data.ssh_url);
+
+exports.createRepoAndClone = (name, parentDir) =>
+  createRepo(name).then((url) => {
+    const projectDir = join(parentDir, name);
+
+    return ensureDir(projectDir)
+      .then(() => simpleGit().clone(url, projectDir))
+      .then(() => {
+        console.log(`Cloned repo to: ${projectDir}`);
+      });
+  });
