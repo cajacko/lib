33fbf4f748c3f9bfbc65e4bd5e60c2e726bcf45e
Charlie Jackson <charlie@charliejackson.com>
2018-07-03 15:45:22 +0100

    Git stuff for the project

--- templates/setupTemplates/templates/Projects.js
+++ templates/setupTemplates/templates/Projects.js
@@ -10,8 +10,8 @@ class Projects extends RunnerTemplate {
     this.runner.add('preRun', [
       () => this.getExistingConfig(),
       () => this.createProjectOrUseDir(),
-      // () => this.getExistingConfig(),
-      // () => this.ensureGitRepo(),
+      () => this.getExistingConfig(),
+      () => this.ensureGitRepo(),
       // () => this.getShouldSetMissingConfigOnly(),
       // () => this.goThroughGenericProjectConfig(),
       // () => this.goThroughExistingTemplatesConfig(),
@@ -37,12 +37,14 @@ class Projects extends RunnerTemplate {
         .then((isGitRepo) => {
           if (isGitRepo) return Promise.resolve();
 
-          return this.runner.prompt({
-            type: 'input',
-            message: `Enter a relative folder name/path for your project, or leave blank to run here:\n${dir}\n`,
-            default: null,
-            // validate: () => {},
-          });
+          return Promise.reject(new Error('Need to define the code here'));
+
+          // return this.runner.prompt({
+          //   type: 'input',
+          //   message: `Enter a relative folder name/path for your project, or leave blank to run here:\n${dir}\n`,
+          //   default: null,
+          //   // validate: () => {},
+          // });
         })
         .then((relativePath) => {
           if (relativePath) {
@@ -52,6 +54,48 @@ class Projects extends RunnerTemplate {
 
           return Promise.resolve();
         }));
+  }
+
+  ensureGitRepo() {
+    return projectDir.get().then(dir =>
+      getIsGitRepo(dir).then((isGitRepo) => {
+        if (isGitRepo) return Promise.resolve();
+
+        return Promise.reject(new Error('Need to implement this step'));
+
+        // console.log("Looks like you are not in a git repo, let's set one up. \nIf you want to run this for an existing project, cd into the project then run this command");
+
+        // // TODO: init git repo now
+        // return setupNewRepo(dir).then(() =>
+        //   this.runner
+        //     .prompt({
+        //       type: 'confirm',
+        //       name: 'shouldAddToGitHub',
+        //       message: 'Do you want to add this as a new repo to GitHub?',
+        //     })
+        //     .then((shouldAddToGithub) => {
+        //       if (!shouldAddToGithub) return Promise.resolve();
+
+        //       return this.runner
+        //         .prompt({
+        //           type: 'input',
+        //           name: 'name',
+        //           message: 'Name for the new repo',
+        //           validate: (name) => {
+        //             if (!isSlug(name)) {
+        //               return 'The repo name must be in a url slug like format e.g. repo-name';
+        //             }
+
+        //             return true;
+        //           },
+        //           default: () => {
+        //             const parts = dir.split('/');
+        //             return parts[parts.length - 1];
+        //           },
+        //         })
+        //         .then(githubName => addToGithub(dir, githubName));
+        //     }));
+      }));
   }
 }
 
