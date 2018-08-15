9ed19d5faf74650b148cf9e80ae2ba8370187fd8
Charlie Jackson <charlie@charliejackson.com>
2018-08-15 16:55:32 +0100

    Setup marketing copy

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,20 +1,43 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
 
-const src = [
+const ignore = [
   '!node_modules',
   '!node_modules/**',
   '!lib/node_modules',
   '!lib/node_modules/**',
-  'lib/**/*.js',
 ];
 
-gulp.task('build', () => {
+const js = ['lib/**/*.js'];
+
+const json = ['lib/**/*.json'];
+
+const watch = ignore.concat(js, json);
+const babelSrc = ignore.concat(js);
+const jsonSrc = ignore.concat(json);
+
+const dest = 'dist';
+
+const getBuildTo = () => {
   const buildArg = process.argv[3];
-  const buildTo = buildArg && buildArg.replace('--', '');
+  return buildArg && buildArg.replace('--', '');
+};
+
+gulp.task('copyJSON', () => {
+  const buildTo = getBuildTo();
+
+  const task = gulp.src(jsonSrc).pipe(gulp.dest(dest));
+
+  if (buildTo) return task.pipe(gulp.dest(buildTo));
+
+  return task;
+});
+
+gulp.task('build', ['copyJSON'], () => {
+  const buildTo = getBuildTo();
 
   const task = gulp
-    .src(src)
+    .src(babelSrc)
     .pipe(babel({
       presets: ['react', 'env', 'flow'],
       plugins: [
@@ -23,13 +46,13 @@ gulp.task('build', () => {
         'babel-plugin-styled-components',
       ],
     }))
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest(dest));
 
   if (buildTo) return task.pipe(gulp.dest(buildTo));
 
   return task;
 });
 
-gulp.task('watch', () => gulp.watch(src, ['build']));
+gulp.task('watch', () => gulp.watch(watch, ['build']));
 
 gulp.task('buildAndWatch', ['build', 'watch']);
