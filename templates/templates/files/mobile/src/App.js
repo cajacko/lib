c8fc00ce62c478c23ee092ea23836c7a99b3492f
Charlie Jackson <charlie@charliejackson.com>
2018-08-15 17:42:13 +0100

    Use the new marketing copy everywhere

--- templates/templates/files/mobile/src/App.js
+++ templates/templates/files/mobile/src/App.js
@@ -1,1 +1,6 @@
+// Bootstrap the project and load in things in the correct order
+
+import './src/config';
+import '@cajacko/lib/dist/utils/bootstrap';
+
 export { default } from './src/components/App';
