49e6cf82e65502f154b0b83e82dd26113c382154
Charlie Jackson <charlie@charliejackson.com>
2018-08-15 17:45:35 +0100

    Dont show react-native error windows

--- templates/templates/files/mobile/src/App.js
+++ templates/templates/files/mobile/src/App.js
@@ -1,6 +1,10 @@
 // Bootstrap the project and load in things in the correct order
 
+import { NativeModules } from 'react-native';
 import './src/config';
 import '@cajacko/lib/dist/utils/bootstrap';
+
+console.disableYellowBox = true;
+NativeModules.ExceptionsManager = null;
 
 export { default } from './src/components/App';
