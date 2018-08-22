d3febe38770b03212e1535840f5cf0ecb4b422d1
Charlie Jackson <charlie@charliejackson.com>
2018-08-22 15:39:27 +0100

    Add bin command to package.json if necessary

--- templates/templates/PackageJSON.js
+++ templates/templates/PackageJSON.js
@@ -1,6 +1,7 @@
 const { RunnerTemplate } = require('@cajacko/template');
 const merge = require('lodash/merge');
 const { readJSON, pathExists } = require('fs-extra');
+const orderObj = require('../utils/orderObj');
 
 const projectJSON = {
   name: 'weewee',
@@ -16,6 +17,17 @@ const projectJSON = {
     precommit: 'lib precommit',
   },
 };
+
+const packageJSONOrder = [
+  'name',
+  'version',
+  'description',
+  'bin',
+  'scripts',
+  'license',
+];
+
+const endPriority = ['dependencies', 'devDependencies'];
 
 class PackageJSON extends RunnerTemplate {
   constructor(...args) {
@@ -44,13 +56,37 @@ class PackageJSON extends RunnerTemplate {
 
   postSetupFiles() {
     if (this.runner.projectConfig) {
-      const { slug, description, license } = this.runner.projectConfig;
+      const {
+        slug,
+        description,
+        license,
+        templates,
+      } = this.runner.projectConfig;
       this.packageJSON.name = slug;
       this.packageJSON.description = description;
       this.packageJSON.license = license;
+
+      if (templates) {
+        Object.keys(templates).forEach((template) => {
+          const { type, bin } = templates[template];
+
+          if (type !== 'npm-module' || !bin) return;
+
+          Object.keys(bin).forEach((command) => {
+            const path = bin[command];
+
+            if (!this.packageJSON.bin) this.packageJSON.bin = {};
+
+            this.packageJSON.bin[command] = path;
+          });
+        });
+      }
     }
 
-    return this.runner.writeJSON(this.packageJSON, 'package.json');
+    return this.runner.writeJSON(
+      orderObj(this.packageJSON, packageJSONOrder, endPriority),
+      'package.json'
+    );
   }
 }
 
