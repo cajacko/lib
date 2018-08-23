f8fc2c1a4c71c391151abd05e782706f707c0241
Charlie Jackson <charlie@charliejackson.com>
2018-08-23 08:42:21 +0100

    Added basic graphql server

--- templates/templates/files/graphql/functions/src/index.js
+++ templates/templates/files/graphql/functions/src/index.js
@@ -1,5 +1,37 @@
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+
 const graphql = (request, response) => {
-  response.send('Hello from graphql!');
+  // Construct a schema, using GraphQL schema language
+  const typeDefs = gql`
+    type Query {
+      hello: String
+    }
+  `;
+
+  // Provide resolver functions for your schema fields
+  const resolvers = {
+    Query: {
+      hello: () => 'Hello world!',
+    },
+  };
+
+  const server = new ApolloServer({ typeDefs, resolvers });
+
+  const app = express();
+
+  server.applyMiddleware({ app });
+
+  // app.get('*', (req, res) => {
+  //   res.send('Hello from Express on Firebase!');
+  // });
+
+  console.log(server.graphqlPath);
+
+  return app(request, response);
+
+  // app.listen({ port: 4000 }, () =>
+  //   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`));
 };
 
 module.exports = graphql;
