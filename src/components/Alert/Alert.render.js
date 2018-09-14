05bf257757e8edc96e3870a2ac9eef5361f72855
Charlie Jackson <charlie@charliejackson.com>
2018-09-14 16:23:48 +0100

    Passed linting on Alert

--- src/components/Alert/Alert.render.js
+++ src/components/Alert/Alert.render.js
@@ -1,8 +1,21 @@
+// @flow
+
 import React from 'react';
 import Text from '../Text';
 import { Div } from '../UI';
 
-const Alert = ({ text }) => (
+type Props = {
+  text: string,
+};
+
+/**
+ * Render a basic alert message
+ *
+ * @param {Object} props The props passed to the component
+ *
+ * @return {ReactElement} The markup to render
+ */
+const Alert = ({ text }: Props) => (
   <Div>
     <Text text={text} />
   </Div>
