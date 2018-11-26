ddaa533fbc81a99151e2348b921352566d7779e6
Charlie Jackson <charlie@charliejackson.com>
2018-11-26 22:37:52 +0000

    🐛 fix(nvm): Allow nvm to run when theres a PREFIX conflict
    - Original Branch: monorepo

--- packages/template-utils/scripts/loadWithNVM.sh
+++ packages/template-utils/scripts/loadWithNVM.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-echo "HERE"
-echo "${@:2}"
+# Sometimes the way we use nvm, it complains about this being set
+unset PREFIX
 
 if [ -d "$NVM_DIR" ]
 then
   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
   [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
   nvm install $1
-  nvm use $1
 fi
 
 echo "${@:2}"
