15e50fe0a28a8cbe74af9e0ef33de2df63ff99f8
Charlie Jackson <charlie@charliejackson.com>
2018-11-26 18:38:32 +0000

    🐛 fix(nvm): Install missing nvm version if it does not exist
    - Original Branch: monorepo

--- packages/template-utils/scripts/loadWithNVM.sh
+++ packages/template-utils/scripts/loadWithNVM.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+echo "HERE"
+echo "${@:2}"
+
 if [ -d "$NVM_DIR" ]
 then
   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
   [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+  nvm install $1
   nvm use $1
 fi
 
