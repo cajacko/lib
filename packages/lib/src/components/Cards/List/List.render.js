38460fab5c8499381be32a41075f12996dadfa56
Charlie Jackson <charlie@charliejackson.com>
2018-12-04 12:36:01 +0000

    ✨ feat(CardsList): Let you specify bottom item props and use a ListITem
    - Original Branch: master

--- packages/lib/src/components/Cards/List/List.render.js
+++ packages/lib/src/components/Cards/List/List.render.js
@@ -7,15 +7,33 @@ import CardsListItem from '../ListItem';
 
 type Props = {
   cards: Cards,
-  renderSectionHeader: RenderSectionHeader,
-  renderItem: () => React.Component<*, *>,
+  bottomItem?: Card,
+  renderSectionHeader?: RenderSectionHeader,
+  renderItem?: () => React.Component<*, *>,
+  renderBottomItem?: () => React.Component<*, *>,
+};
+
+const defaultProps = {
+  bottomItem: null,
+  renderSectionHeader: null,
+  renderItem: null,
+  renderBottomItem: null,
+};
+
+type RenderItemProps = {
+  item: Card & {
+    _renderCustom?: Card => React.Component<*, *>,
+  },
 };
 
 /**
  * The default render item func, just passes all info through to the card list
  * item
  */
-const RenderItem = ({ item }: { item: Card }) => <CardsListItem {...item} />;
+const RenderItem = ({
+  item: { _renderCustom, ...itemProps },
+}: RenderItemProps) =>
+  (_renderCustom ? _renderCustom(itemProps) : <CardsListItem {...itemProps} />);
 
 /**
  * Display a list of cards
@@ -24,20 +42,37 @@ const CardsList = ({
   cards,
   renderSectionHeader,
   renderItem,
+  renderBottomItem,
+  bottomItem,
   ...props
 }: Props) => {
   const finalRenderItem = renderItem || RenderItem;
+  const finalCards = cards.slice();
+
+  if (renderBottomItem) {
+    finalCards.push({
+      _renderCustom: renderBottomItem,
+      key: '_customBottomRender',
+    });
+  } else if (bottomItem) {
+    finalCards.push({
+      key: '_customBottomRender',
+      ...bottomItem,
+    });
+  }
 
   return renderSectionHeader ? (
     <SectionList
       renderSectionHeader={renderSectionHeader}
-      sections={cards}
+      sections={finalCards}
       renderItem={finalRenderItem}
       {...props}
     />
   ) : (
-    <FlatList data={cards} renderItem={finalRenderItem} {...props} />
+    <FlatList data={finalCards} renderItem={finalRenderItem} {...props} />
   );
 };
+
+CardsList.defaultProps = defaultProps;
 
 export default CardsList;
