f5a1b4b8b8427fcb8051e8c1aeef3bbcc9d99c47
Charlie Jackson <charlie@charliejackson.com>
2018-12-11 13:06:07 +0000

    🛠 refactor(CardsList): Use standard list header/footer
    - Original Branch: improv-stories/template-init

--- packages/lib/src/components/Cards/List/List.render.js
+++ packages/lib/src/components/Cards/List/List.render.js
@@ -3,7 +3,6 @@
 import React from 'react';
 import { FlatList, SectionList } from '../../UI';
 import type { Cards, Card, RenderSectionHeader } from '../types';
-import CardsListItem from '../ListItem';
 
 type Props = {
   cards: Cards,
@@ -14,6 +13,8 @@ type Props = {
   keyExtractor?: Card => string,
   bottomPadding?: boolean | number,
   innerRef?: () => void,
+  bottomItemProps?: { [string]: * },
+  topItemProps?: { [string]: * },
 };
 
 const defaultProps = {
@@ -24,33 +25,8 @@ const defaultProps = {
   keyExtractor: null,
   bottomPadding: null,
   innerRef: null,
-};
-
-type RenderItemProps = {
-  item: Card & {
-    key: string,
-    _renderCustom?: Card => React.Component<*, *>,
-  },
-};
-
-/**
- * The default render item func, just passes all info through to the card list
- * item
- */
-const withRenderItem = renderItem => (props: RenderItemProps) => {
-  const { item } = props;
-
-  if (props.item._renderCustom) return props.item._renderCustom(props);
-
-  if (item.key === '_customBottomRender' || item.key === '_customTopRender') {
-    return <CardsListItem {...item} />;
-  }
-
-  if (typeof renderItem === 'function') {
-    return renderItem(props);
-  }
-
-  return <CardsListItem {...item} />;
+  bottomItemProps: {},
+  topItemProps: {},
 };
 
 /**
@@ -65,68 +41,29 @@ const contentContainerStyle = ({ bottomPadding }) => {
 };
 
 /**
- * Wrap around the key extractor if one is provided. As we don't want it to
- * run for custom items we add in here
- */
-const customKeyExtractor = keyExtractor => (item) => {
-  // Don't want to run a custom key extractor for the bottom/top render, as we
-  // pass they key in ourselves
-  if (item.key === '_customBottomRender' || item.key === '_customTopRender') {
-    return item.key;
-  }
-
-  return keyExtractor(item);
-};
-
-/**
  * Display a list of cards
  */
 const CardsList = ({
   cards,
-  renderSectionHeader,
-  renderItem,
-  bottomItem,
-  keyExtractor,
   bottomPadding,
+  renderSectionHeader,
   innerRef,
-  topItem,
   ...props
 }: Props) => {
-  const finalCards = cards.slice();
-  const finalKeyExtractor = keyExtractor && customKeyExtractor(keyExtractor);
-
-  if (bottomItem) {
-    finalCards.push({
-      _renderCustom: bottomItem,
-      key: '_customBottomRender',
-    });
-  }
-
-  if (topItem) {
-    finalCards.unshift({
-      _renderCustom: topItem,
-      key: '_customTopRender',
-    });
-  }
-
   const contentContainerStyles = contentContainerStyle({ bottomPadding });
 
   return renderSectionHeader ? (
     <SectionList
       ref={innerRef}
       renderSectionHeader={renderSectionHeader}
-      sections={finalCards}
-      renderItem={withRenderItem(renderItem)}
-      keyExtractor={finalKeyExtractor}
+      sections={cards}
       contentContainerStyle={contentContainerStyles}
       {...props}
     />
   ) : (
     <FlatList
       ref={innerRef}
-      data={finalCards}
-      renderItem={withRenderItem(renderItem)}
-      keyExtractor={finalKeyExtractor}
+      data={cards}
       contentContainerStyle={contentContainerStyles}
       {...props}
     />
