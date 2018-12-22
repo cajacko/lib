5f99eff979981d84a29e7150220969e38c925085
Charlie Jackson <charlie@charliejackson.com>
2018-12-22 09:55:21 +0000

    🐛 fix(publish): Test travis with no env
    - Original Branch: improv-stories/template-init

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -6,16 +6,16 @@ set -e
 command="docker create"
 
 # Get all the env keys and pass in from the host
-str=$(egrep -v '^#' .env | xargs)
-IFS=" "
-ary=($str)
-for key in "${!ary[@]}";
-do
-  str2="${ary[$key]}"
-  IFS="="
-  ary2=($str2)
-  command="${command} -e ${ary2[0]}"
-done
+# str=$(egrep -v '^#' .env | xargs)
+# IFS=" "
+# ary=($str)
+# for key in "${!ary[@]}";
+# do
+#   str2="${ary[$key]}"
+#   IFS="="
+#   ary2=($str2)
+#   command="${command} -e ${ary2[0]}"
+# done
 
 command="${command} -it --name=publish node:6.15.1-alpine sh /App/scripts/runPublish.sh"
 
